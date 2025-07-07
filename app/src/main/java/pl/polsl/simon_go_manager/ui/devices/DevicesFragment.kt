deeef8e69719e8f72a46470979946b6eb2f5b397
JanRakwic <jan.rakwic@itsilesia.com>
2025-07-07 11:43:42 +0200

    zaktualizowano nawigacje: zamieniono dashboard i notifications na devices i settings

--- app/src/main/java/pl/polsl/simon_go_manager/ui/devices/DevicesFragment.kt
+++ app/src/main/java/pl/polsl/simon_go_manager/ui/devices/DevicesFragment.kt
@@ -1,4 +1,4 @@
-package pl.polsl.simon_go_manager.ui.notifications
+package pl.polsl.simon_go_manager.ui.devices
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import pl.polsl.simon_go_manager.databinding.FragmentNotificationsBinding
+import pl.polsl.simon_go_manager.databinding.FragmentDevicesBinding
 
-class NotificationsFragment : Fragment() {
+class DevicesFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentDevicesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val devicesViewModel =
+            ViewModelProvider(this).get(DevicesViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDevicesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        devicesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/pl/polsl/simon_go_manager/ui/devices/DevicesViewModel.kt
+++ app/src/main/java/pl/polsl/simon_go_manager/ui/devices/DevicesViewModel.kt
@@ -1,13 +1,13 @@
-package pl.polsl.simon_go_manager.ui.dashboard
+package pl.polsl.simon_go_manager.ui.devices
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class DevicesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is devices Fragment"
     }
     val text: LiveData<String> = _text
 }
